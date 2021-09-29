a640df53a5e28ce46398cd16ae09db127e9a9d74
Manav Misra <manavm@visionwebsoft.com>
2021-09-28 23:46:23 -0500

    Add model for product

---
+++ server/app/model.js
@@ -0,0 +1,39 @@
+// Faux `enum`
+const categories = ["Sporting Goods", "Electronics"];
+
+const validate = ({ category, price, stocked = true, name = "" } = {}) => {
+  const ret = [];
+
+  if (!categories.includes(category)) {
+    ret.push("Invalid category");
+  }
+
+  if (price < 0.01) {
+    ret.push("Price must be non-zero");
+  }
+
+  if (typeof stocked !== "boolean") {
+    ret.push("Invalid stocked");
+  }
+
+  if (!name.length) {
+    ret.push("Name must be non-empty");
+  }
+
+  return ret;
+};
+
+const withStringifiedPrice = ({ price, ...rest }) => ({
+  ...rest,
+  price: `$${price.toFixed(2)}`,
+});
+
+export default (newProduct) => {
+  const errors = validate(newProduct);
+  if (errors.length) {
+    throw new Error(errors.join("\n"));
+  }
+
+  // If no errors, just need to stringify the price
+  return withStringifiedPrice(newProduct);
+};
