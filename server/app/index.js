b2f1b6495cc0c82d3258864fb7e772e47d47aac6
Manav Misra <manavm@visionwebsoft.com>
2021-09-29 09:06:47 -0500

    :coffin: Hello World route

--- server/app/index.js
+++ server/app/index.js
@@ -3,10 +3,6 @@ import config from "./config.js";
 
 const app = express();
 
-app.get("/", (_, res) => {
-  res.send("Hello World");
-});
-
 app.use(express.json());
 
 app.listen(config.port, () => {
