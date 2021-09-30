c102240587452f4328f9a769fea164b9e34ef9f1
Manav Misra <manavm@visionwebsoft.com>
2021-09-30 07:17:48 -0500

    :card_file_box: Delete 🔥 data b4 seeding 🌱

--- server/app/db/seed/index.js
+++ server/app/db/seed/index.js
@@ -5,7 +5,9 @@ import productData from "./productData.json";
 (async () => {
   try {
     const conn = await client.connect();
-    await conn.db(config.dbName).collection("products").insertMany(productData);
+    const coll = conn.db(config.dbName).collection("products");
+    await coll.deleteMany({});
+    await coll.insertMany(productData);
     await conn.close();
     console.info("Products 🏬 seeded! 🌱 🗃️ ");
   } catch (err) {
