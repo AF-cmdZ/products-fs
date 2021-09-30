def053065bf23477446d68116e9df874de3729b2
Manav Misra <manavm@visionwebsoft.com>
2021-09-30 13:42:07 -0500

    Log Admin in and Out

--- client/src/views/HomeView/index.jsx
+++ client/src/views/HomeView/index.jsx
@@ -1,17 +1,31 @@
-import FilterableProductTable from "./FilterableProductTable";
 import React from "react";
 import { Link } from "react-router-dom";
+import FilterableProductTable from "./FilterableProductTable";
 
 function HomeView() {
+  const handleClick = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
+
   return (
     <>
       <FilterableProductTable />
-      <Link
-        to="/admin"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 w-min"
-      >
-        Admin
-      </Link>
+      {localStorage.getItem("token") ? (
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4 w-max"
+          onClick={handleClick}
+        >
+          Log Out
+        </button>
+      ) : (
+        <Link
+          to="/admin"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 w-max"
+        >
+          Admin
+        </Link>
+      )}
     </>
   );
 }
