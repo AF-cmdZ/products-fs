9f18a9440c0dd0c4e6e8fb5a0e84cfaf3e7ef3d6
Manav Misra <manavm@visionwebsoft.com>
2021-09-30 15:12:41 -0500

    Conditionally render UDButtons

--- client/src/views/HomeView/FilterableProductTable/ProductTable/ProductRow.jsx
+++ client/src/views/HomeView/FilterableProductTable/ProductTable/ProductRow.jsx
@@ -1,11 +1,31 @@
+import AdminContext from "@app/context/AdminContext";
 import PropTypes from "prop-types";
 import React from "react";
 
 function ProductRow({ product: { _id, name, price } }) {
+  const [isAdmin] = React.useContext(AdminContext);
+
+  const renderUDBtns = () =>
+    isAdmin && (
+      <>
+        <td>
+          <button className="btn bg-red-500 text-white p-1 text-xs rounded-lg">
+            🔥 Delete
+          </button>
+        </td>
+        <td>
+          <button className="btn bg-yellow-500 text-black p-1 text-xs rounded-lg">
+            ✨ Update
+          </button>
+        </td>
+      </>
+    );
+
   return (
     <tr id={_id}>
       <td>{name}</td>
       <td>{price}</td>
+      {renderUDBtns()}
     </tr>
   );
 }
