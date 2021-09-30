736aae065c87f42dc3121175dce71bfd0dc3ec88
Manav Misra <manavm@visionwebsoft.com>
2021-09-30 07:18:01 -0500

    Write resolvers

---
+++ server/app/graphql/resolvers.js
@@ -0,0 +1,28 @@
+import productController from "../controller.js";
+import createProduct from "../model.js";
+
+export default {
+  Query: {
+    products() {
+      return productController.index();
+    },
+  },
+  Mutation: {
+    // Destructure args into product object
+    async createProduct(_, { product }) {
+      const validatedProduct = createProduct(product);
+      const { insertedId: _id } = await productController.create(
+        validatedProduct
+      );
+      return { ...validatedProduct, _id };
+    },
+    async updateProduct(_, { product }) {
+      const validatedProduct = createProduct(product);
+      await productController.update(validatedProduct);
+      return validatedProduct;
+    },
+    deleteProduct(_, { id }) {
+      return productController.delete(id);
+    },
+  },
+};
