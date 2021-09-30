7ca544dd8a5a7dd824ff54874fe040a8c92fe110
Manav Misra <manavm@visionwebsoft.com>
2021-09-30 10:07:12 -0500

    :construction: Pass dummy handlers

--- client/src/components/FilterableProductTable/index.jsx
+++ client/src/components/FilterableProductTable/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 
+function handleToggle(e) {
+  console.log("toggle", e.target.checked);
+}
+
+function handleSearch(e) {
+  e.preventDefault();
+  const query = e.target.value;
+  console.log(query);
+}
+
 function FilterableProductTable() {
   return (
     <div>
-      <SearchBar />
+      <SearchBar checkboxHandler={handleToggle} inputHandler={handleSearch} />
     </div>
   );
 }
